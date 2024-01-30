58112c3f96bd76b9381744d2d50bfb223a0dbc08
Zelei Cheng <38622719+chengzelei@users.noreply.github.com>
2024-01-30 00:40:52 -0600

    Update retrain.sh

--- Retrain_mujoco/retrain.sh
+++ Retrain_mujoco/retrain.sh
@@ -1,10 +1,10 @@
-python -u retrain.py --env "HalfCheetah-v3"\
-                     --agent_path "/home/zck7060/new_code/baseline/best_model"\
-                     --masknet_path "/home/zck7060/new_code/masknet/best_model"\
-                     --vec_norm_path "/home/zck7060/new_code/baseline/vec_normalize.pkl"\
+python -u retrain.py --env "Hopper-v3"\
+                     --agent_path "baseline/best_model"\
+                     --masknet_path "masknet/best_model"\
+                     --vec_norm_path "baseline/vec_normalize.pkl"\
                      --n_envs 20\
                      --seed 0\
-                     --go_prob 0.5\
+                     --go_prob 1\
                      --check_freq 200\
-                     --bonus 'e3b'\
-                     --bonus_scale 0.01
+                     --bonus 'rnd'\
+                     --bonus_scale 0
